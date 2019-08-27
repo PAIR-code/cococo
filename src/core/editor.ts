5f2678bc20d29a91f850035c264c2e1f8a5bdadf
Ryan Louie <rlouie@google.com>
2019-08-27 02:52:56 -0700

    change default contrainToKey to true

--- src/core/editor.ts
+++ src/core/editor.ts
@@ -119,7 +119,7 @@ class Editor {
     this._mode = mode;
   }
 
-  @observable private _constrainToKey = false;
+  @observable private _constrainToKey = true;
   @computed get constrainToKey() {
     return this._constrainToKey;
   }
